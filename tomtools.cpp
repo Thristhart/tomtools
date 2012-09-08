5cea37eece9840c30fb22a006d5e170afd864ebd
Tom Shea <tom@shea.at>
2012-09-08 18:10:29 +0000

    Initial commit

---
+++ tomtools.cpp
@@ -0,0 +1,102 @@
+#include "tomtools.h"
+#include <iostream>
+
+// platform-specific stuff needed for termWidth()
+#ifdef __linux__
+	#include <sys/ioctl.h>
+	#include <unistd.h>
+#elif defined _WIN32 || _WIN64
+	#include <windows.h>
+#endif
+
+using namespace std;
+int promptInt(string prompt) {
+	int input = 0;
+	cout << prompt + " ";
+	cin >> input;
+	return input;
+}
+double promptDouble(string prompt) { 
+	double input = 0;
+	cout << prompt + " ";
+	cin >> input;
+	return input;
+}
+float promptFloat(string prompt) {
+	float input = 0;
+	cout << prompt + " ";
+	cin >> input;
+	return input;
+}
+string promptString(string prompt) {
+	string input;
+	cout << prompt + " ";
+	getline(cin, input);
+	return input;
+}
+bool promptYN(string prompt) {
+	while(true) {
+		string textIn = promptString(prompt +  " [yes/no]");
+		if(textIn == "y" || textIn == "yes" || textIn == "1")
+			return true;
+		if(textIn == "n" || textIn == "no" || textIn == "0")
+			return false;
+		cout << "Invalid input. Valid inputs include: yes, no, y, n, 1, 0." << endl;
+	}
+}
+
+void boxBanner(string text, char decoration) {
+	fillLine(decoration);
+	banner(text, decoration);
+	fillLine(decoration);
+}
+
+void banner(string text, char decoration) {
+	int width = termWidth();
+	int bufferSize = width / 2 - 1 - text.length() / 2;
+
+	cout << decoration;
+	space(bufferSize);
+	cout << text;
+	space(bufferSize);
+	cout << decoration;
+	cout << endl;
+}
+void center(string text) {
+	int width = termWidth();
+	int length = text.length();
+
+	space(width / 2 - length / 2);
+
+	cout << text << endl;
+}
+void space(int count) {
+	repeatChar(' ', count);
+}
+void repeatChar(char fill, int size) {
+	for(int i = 0; i < size; i++) {
+		cout << fill;
+	}
+}
+void fillLine(char fill) {
+	repeatChar(fill, termWidth());
+	cout << endl;
+}
+int termWidth() {
+	#ifdef __linux__
+	// http://www.delorie.com/djgpp/doc/libc/libc_495.html
+	struct winsize w;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+	return w.ws_col;
+					
+	#elif defined _WIN32 || _WIN64
+					
+	// http://msdn.microsoft.com/en-us/library/windows/desktop/ms683171%28v=VS.85%29.aspx
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	if(!GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+		cerr << "Don't have permission to read the console screen buffer info." << endl;
+	else
+		return csbi.srWindow.Right-csbi.srWindow.Left;
+					
+	#endif
+}
---
+++ tomtools.h
@@ -0,0 +1,47 @@
+/* Name: Tom's Tools
+ * Author: Tom Shea
+ * Behavior: Various helpful functions, mostly for interacting with the
+ * user via command line.
+ */
+#include <string>
+using namespace std;
+
+// include guard
+#ifndef TOMTOOLS_INCLUDED
+#define TOMTOOLS_INCLUDED
+
+// various prompting types
+// output the prompt, and input the specific type
+int promptInt(string prompt);
+double promptDouble(string prompt);
+float promptFloat(string prompt);
+string promptString(string prompt);
+// yes/no questions
+bool promptYN(string prompt);
+
+// various outputs
+
+// Centered text surrounded by the decoration
+// text: "Hello World" decoration: '#' would look like
+// #####################################################
+// #                    Hello World                    #
+// #####################################################
+void boxBanner(string text, char decoration);
+// Centered text flanked by the decoration
+// #                    Hello World                    #
+void banner(string output, char decoration);
+// Centered text
+//                      Hello World                     
+void center(string text);
+// the width in characters of the terminal, calculated using platform-specific calls
+// doesn't work on macs
+int termWidth();
+// write <count> number of ' ' to cout
+void space(int count);
+// repeat the given char size times
+void repeatChar(char fill, int size);
+// write a line of the given char
+// ####################################################
+void fillLine(char fill);
+
+#endif
