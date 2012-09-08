e3cf9656d38c82d7afa0c32da2d20b5e4ec770d4
Tom Shea <tom@shea.at>
2012-09-08 18:29:44 +0000

    Adding an example of prompting

---
+++ examples/prompt.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "../tomtools.h"
+#include <string>
+
+using namespace std;
+
+int main() {
+	string name = promptString("What's your name?");
+	cout << "Hello, " + name + ".\n";
+	int age = promptInt("How old are you?");
+	double gpa = promptDouble("What's your GPA?");
+	
+	cout << "Your name is " + name + " and you are " << age << " years old, with a GPA of " << gpa << endl;
+	if(promptYN("Is that correct?"))
+		cout << "Yay!" << endl;
+	else
+		cout << "Aw. Are you sure?" << endl;
+}
