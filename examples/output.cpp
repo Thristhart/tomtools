ee93ecede6c00610bf3e927933f62e0b88ba2b71
Tom Shea <tom@shea.at>
2012-09-10 18:57:50 +0000

    Adding output example

---
+++ examples/output.cpp
@@ -0,0 +1,16 @@
+#include "../tomtools.h"
+
+int main() {
+	print("Hello World!");	
+	print("");
+
+	center("Hello World!");
+	print("");
+
+	banner("Hello World!", '#');
+	print("");
+
+	boxBanner("Hello World!", '#');
+
+	return 0;
+}
